25b9fc718342b41ba87348d376082809254958a7
sonyt <sonyt@LAPTOP-0CCRI3TF>
2021-11-23 13:14:26 -0500

    Add project files.

---
+++ TodoList/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// created new class to the root of project
+namespace TodoList
+{
+    // it will contain 2 strings to public properties of title and a bollean IsDone
+    public class TodoItem
+    {
+        public string Title { get; set; }
+// for checkbox if the task is done
+        public bool IsDone { get; set; }
+    }
+}
